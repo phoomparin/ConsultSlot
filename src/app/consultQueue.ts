917646d6bd9034e4f36b475617911baf1ba18186
Phoomparin Mano <phoomparin@gmail.com>
2019-06-09 18:53:57 +0700

    :sparkles: Update consult mapping

--- src/app/consultQueue.ts
+++ src/app/consultQueue.ts
@@ -12,28 +12,27 @@ export type ConsultMap = {
 
 export const queues: ConsultMap = {
   '18:20 - 18:40': {
-    "Gunman and the Witch": "บอนนี่, จั่น, ปรอ   ",
-    "Paper Piano": "Good Factory",
+    "Gunman and the Witch": "บอนนี่, จั่น, ปรอ",
+    "Paper Piano": "จั่น นิ้งหน่อง นิ",
     "Pluk Kla": "ฟี่, บวบ"
   },
   "18:40 - 19:00": {
     "Kings of Dungeon": "บอนนี่",
-    "What's the Fish?": "Good Factory, จั่น, ปรอ",
-    "iCrab": "บวบ"
+    "Super Shrimp": "นิ้งหน่อง นิ",
+    "iCrab": "บวบ ฟี่"
   },
   '19:00 - 19:20': {
-    "Big Boss": "บอนนี่",
-    "จันทรทัศน์ (Lunar Wheel)": "นิ, บวบ",
-    "มีดกรีดยางสายฟ้า": "ปรอ, ฟี่, จั่น",
-    "Super Shrimp": "พฤศ, นิ้งหน่อง"
+    "Big Boss": "บอนนี่ นิ้งหน่อง",
+    "จันทรทัศน์ (Lunar Wheel)": "นิ บวบ พฤศ",
+    "มีดกรีดยางสายฟ้า": "ปรอ ฟี่ จั่น",
   },
   "19:20 - 19:40": {
-    "Million Words (PPLG)": "Good Factory, บอนนี่",
-    "Intelligent Toilet System (ห้องน้ำเด็กช่าง)": "ฟี่, บวบ",
-    "Soft Silk": "Good Factory"
+    "Million Words (PPLG)": "บอนนี่ พฤศ",
+    "Intelligent Toilet System (ห้องน้ำเด็กช่าง)": "ฟี่ บวบ",
+    "Soft Silk": "จั่น นิ้งหน่อง"
   },
   "19:40 - 20:00": {
-    "Bloody Buddy": "บอนนี่",
-    "Clean Oyster": "ปรอ, ฟี่",
+    "Bloody Buddy": "บอนนี่ นิ",
+    "Clean Oyster": "ปรอ ฟี่",
   },
 }
